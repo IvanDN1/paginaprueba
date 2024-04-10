f814a973a18f5e027e6c9059b4750d4eb823b1d7
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-04-10 15:38:32 -0400

    nada

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link href="/resources/css/navbar.css" rel="stylesheet">
+    <link href="/resources/css/navbar.css" rel="stylesheet" type="text/css">
     <!-- Estilos de Bootstrap -->
 
 </head>
