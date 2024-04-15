7a9845fa4e1f443b647b1e7ebc612852f25e87f2
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-04-15 13:33:49 -0400

    cambios de estilos

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,14 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/nosotros.css') }}" rel="stylesheet">
+
     <title>{{ $title ?? 'Mi Sitio Web' }}</title>
 </head>
 <body>
     @include('layout.navbar')
-    @include('layout.header')
-    <div class="container">
-        @yield('content')
-    </div>
+        <div>
+            @include('layout.header')
+                <div class="container">
+                    @yield('content')
+                </div>
+        </div>
     @include('layout.footer')
 </body>
 </html>
