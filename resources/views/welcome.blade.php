024bcba31cdf62932874b16dc94df74a103bc3a4
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-05-23 17:34:19 -0400

    configuracion e imagenes nosotros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.layout')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <section class="bg-gradient-primary-to-secondary3 py-2">
         <div class="container px-4 my-4">
             <h1 class="h1">TM corporación</h1>
@@ -15,25 +15,78 @@
         </div>
     </section>
     <section class="bg-gradient-primary-to-secondary4 py-4">
-        <div class="container px-1 my-1">
-            <p>
-                Diseñamos capacitaciones y asesorías para la comunidad educativa del país Buscamos inspirar a docentes, administrativos y estudiantes fomentando la curiosidad y el pensamiento crítico fortaleciendo e impulsando a la comunidad educativa a evolucionar.
-            <br>
-                No hay misión más clara que entregar excelencia y calidad a la educación a través de las soluciones innovadoras, personalizadas y estrechamente vinculadas hacia las personas.
-            <br>
-                Al disponer de nuestro servicios también obtienen nuestra disposición a la implementación de nuevas tecnologías y metodologías para la educación, de manera más efectiva buscando abordar los desafíos actuales que se imponen a día de hoy.
-            <br>
-                Responsabilidad como uno de nuestros pilares mas fundamentales, encaminándonos hacia la excelencia, valorando la capacidad de adaptación, el trabajo duro, la resiliencia y perseverancia en la búsqueda de nuevos conocimientos, habilidades y estrategias.
-            <br>
-                Promoviendo la aplicación de las habilidades en contextos prácticos, reales y controlados a través de la cooperación y respeto mutuo, esforzándonos por alcanzar los altos estándares de calidad y eficiencia, de manera positiva aportando a la sociedad.
-            </p>
-        </div>
-    </section>
-    <section class="bg-gradient-primary-to-secondary5 py-5">
-        <div class="container px-4 my-4">
-            <a href="/" class="row ">
-            <img src="/images/LogoATE.png" alt="Slogan empresa" id="SloganATE">
-        </div>
+        <div class="container-fluid">
+            <div class="row mb-2 text-center">
+                <div class="col-6 themed-grid-col">
+                    <p>
+                    Diseñamos capacitaciones y asesorías para la comunidad educativa del país
+                    buscamos inspirar a docentes,
+                    administrativos y estudiantes fomentando la curiosidad y
+                    el pensamiento crítico fortaleciendo e impulsando a la comunidad educativa a evolucionar.
+                    </p>
+                </div>
+                <div class="col-6 themed-grid-col">
+                    <a>
+                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                    </a>
+                </div>
+            </div>
+            <div class="row mb-2 text-center">
+                <div class="col-6 themed-grid-col">
+                    <a>
+                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                    </a>
+                </div>
+                <div class="col-6 themed-grid-col">
+                    Al disponer de nuestro servicios también obtienen nuestra disposición
+                    a la implementación de nuevas tecnologías y metodologías para la educación,
+                    de manera más efectiva buscando abordar los desafíos actuales que se imponen a día de hoy.
+                </div>
+            </div>
+            <div class="row mb-2 text-center">
+                <div class="col-6 themed-grid-col">
+                    No hay misión más clara que entregar excelencia y
+                    calidad a la educación a través de las soluciones innovadoras,
+                    personalizadas y estrechamente vinculadas hacia las personas.
+                </div>
+                <div class="col-6 themed-grid-col">
+                    <a>
+                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                    </a>
+                </div>
+            </div>
+            <div class="row mb-2 text-center">
+                <div class="col-6 themed-grid-col">
+                    <a>
+                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                    </a>
+                </div>
+                <div class="col-6 themed-grid-col">
+                    Promoviendo la aplicación de las habilidades en contextos prácticos,
+                    reales y controlados a través de la cooperación y respeto mutuo,
+                    esforzándonos por alcanzar los altos estándares de calidad y eficiencia,
+                    de manera positiva aportando a la sociedad.
+                </div>
+            </div>
+            <div class="row mb-2 text-center">
+            <div class="col-6 themed-grid-col">
+                Responsabilidad como uno de nuestros pilares mas fundamentales,
+                encaminándonos hacia la excelencia, valorando la capacidad de adaptación,
+                el trabajo duro, la resiliencia y perseverancia en la búsqueda de nuevos conocimientos,
+                habilidades y estrategias.
+            </div>
+                <div class="col-6 themed-grid-col">
+                    <a>
+                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                    </a>
+                </div>
+            </div>
+        <section class="bg-gradient-primary-to-secondary5 py-5">
+            <div class="container px-4 my-4">
+                <a href="/" class="row ">
+                <img src="/images/LogoATE.png" alt="Slogan empresa" id="SloganATE">
+            </div>
+        </section>
     </section>
     <section class=" bg-gradient-primary-to-secondary6 text-white py-3">
         <div class="container px-5 my-5">
