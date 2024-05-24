18cd6703abd3ff2cf6ae4b3dffd9ed181b61c9b4
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-05-24 10:45:45 -0400

    prueba de Mail

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
     <section class="bg-gradient-primary-to-secondary3 py-2">
         <div class="container px-4 my-4">
-            <h1 class="h1">TM corporación</h1>
+                <img src="/images/LogoATE2.png" class="aling-center">
             <h2 class="h2">
                 Explorando nuestros servicios, acompañamos a transformar la educación en comunidad.
             <br>
@@ -16,7 +16,7 @@
     </section>
     <section class="bg-gradient-primary-to-secondary4 py-4">
         <div class="container-fluid">
-            <div class="row mb-2 text-center">
+            <div class="row mb-6 text-center">
                 <div class="col-6 themed-grid-col">
                     <p>
                     Diseñamos capacitaciones y asesorías para la comunidad educativa del país
@@ -31,49 +31,56 @@
                     </a>
                 </div>
             </div>
-            <div class="row mb-2 text-center">
+            <div class="row mb-6 text-center">
                 <div class="col-6 themed-grid-col">
                     <a>
                         <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
                     </a>
                 </div>
                 <div class="col-6 themed-grid-col">
-                    Al disponer de nuestro servicios también obtienen nuestra disposición
+                    <p>
+                    Disponemos nuestros servicios dando disposición
                     a la implementación de nuevas tecnologías y metodologías para la educación,
                     de manera más efectiva buscando abordar los desafíos actuales que se imponen a día de hoy.
+                    </p>
                 </div>
             </div>
-            <div class="row mb-2 text-center">
+            <div class="row mb-6 text-center">
                 <div class="col-6 themed-grid-col">
-                    No hay misión más clara que entregar excelencia y
+                    <p>
+                    No tenemos misión más clara que entregar excelencia y
                     calidad a la educación a través de las soluciones innovadoras,
                     personalizadas y estrechamente vinculadas hacia las personas.
+                    </p>
                 </div>
                 <div class="col-6 themed-grid-col">
                     <a>
-                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                        <img src="/images/mision.jpg" alt="imagen1"  class="mi-clase" id="img1" >
                     </a>
                 </div>
             </div>
-            <div class="row mb-2 text-center">
+            <div class="row mb-6 text-center">
                 <div class="col-6 themed-grid-col">
                     <a>
-                        <img src="/images/diseñando.jpg" alt="imagen1"  class="mi-clase" id="img1" >
+                        <img src="/images/cooperacion.jpg" alt="imagen1"  class="mi-clase" id="img1" >
                     </a>
                 </div>
                 <div class="col-6 themed-grid-col">
-                    Promoviendo la aplicación de las habilidades en contextos prácticos,
-                    reales y controlados a través de la cooperación y respeto mutuo,
-                    esforzándonos por alcanzar los altos estándares de calidad y eficiencia,
-                    de manera positiva aportando a la sociedad.
+                    <p>
+                    Promovemos la aplicación de las habilidades en contextos prácticos reales y controlados,
+                    a través de la cooperación y respeto mutuo esforzándonos por alcanza altos estándares de calidad y eficiencia,
+                    buscado aportar asociedad de manera positiva a travez de nuestro servicio.
+                    </p>
                 </div>
             </div>
             <div class="row mb-2 text-center">
             <div class="col-6 themed-grid-col">
-                Responsabilidad como uno de nuestros pilares mas fundamentales,
+                <p>
+                La responsabilidad es uno de nuestros pilares mas fundamentales,
                 encaminándonos hacia la excelencia, valorando la capacidad de adaptación,
                 el trabajo duro, la resiliencia y perseverancia en la búsqueda de nuevos conocimientos,
                 habilidades y estrategias.
+                </p>
             </div>
                 <div class="col-6 themed-grid-col">
                     <a>
@@ -81,12 +88,7 @@
                     </a>
                 </div>
             </div>
-        <section class="bg-gradient-primary-to-secondary5 py-5">
-            <div class="container px-4 my-4">
-                <a href="/" class="row ">
-                <img src="/images/LogoATE.png" alt="Slogan empresa" id="SloganATE">
-            </div>
-        </section>
+
     </section>
     <section class=" bg-gradient-primary-to-secondary6 text-white py-3">
         <div class="container px-5 my-5">
