0d4d821f5158040d753d64516629be2bed2a0033
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-06-14 05:18:25 -0400

    modificaciones

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,31 +1,8 @@
-
-    <link href="{{ asset('css/app.scss') }}" rel="stylesheet">
-    <link href="{{ asset('css/footer.scss') }}" rel="stylesheet">
-
-<footer class="footer bg-gradient-primary-to-secondary7">
-    <div class="container">
-        <div class="row">
-            <div class="col-6 col-md-2 mb-3 custom-section align-rigth">
-                <ul class="nav flex-column">
-                    <ul class="nav flex-column">
-                    <li class="nav-item"><a href="/" class="nav-link p-0 text-body-secondary nav-link-custom"><H6 class="mih6">INICIO</H6></a></li>
-                    <li class="nav-item"><a href="/cursos" class="nav-link p-0 text-body-secondary nav-link-custom"><H6 class="mih6">CURSOS</H6></a></li>
-                    <li class="nav-item"><a href="/contact" class="nav-link p-0 text-body-secondary nav-link-custom"><H6 class="mih6">CONTACTO</H6></a></li>
-                    <li class="nav-item"><a href="/nosotros" class="nav-link p-0 text-body-secondary nav-link-custom"><H6 class="mih6">NOSOTROS</H6></a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md-2 mb-3 custom-section">
-                <ul class="nav flex-column">
-                    <li class="nav-item"><a href="https://tmcapacitacion.cl/" class="nav-link p-0 text-body-secondary nav-link-custom"><H6>TMCAPACITACIÓN</H6></a></li>
-                    <li class="nav-item"><a href="/contact" class="nav-link p-0 text-body-secondary nav-link-custom"><H6>CONTACTO</H6></a></li>
-                    <li class="nav-item"><a href="/nosotros" class="nav-link p-0 text-body-secondary nav-link-custom"><H6>NOSOTROS</H6></a></li>
-                </ul>
-            </div>
-            <div class="d-flex flex-column flex-sm-row justify-content-between py-0 my-2 border-top">
-                <div class="col-md-4 d-flex align-items-center">
-                    <span class="mb-2 mb-md-0 custom-span">©2024 TMCorporación, Inc</span>
-                </div>
-            </div>
-        </div>
-    </div
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+    <li class="nav-item"><a href="/nosotros" class="nav-link px-2 text-body-secondary">Nosotros</a></li>
+    <li class="nav-item"><a href="/contact" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2024 TMCorporación, Inc</p>
 </footer>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,22 +2,23 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('js/navbar.js') }}" rel="stylesheet">
 
-
-    <nav class="navbar navbar-expand-lg navbar-custom bg-gradient-primary-to-secondary" id="navbarResponsive">
-        <div class="container-fluid">
-            <a href="https://www.tmcorporacion.cl/" class="navbar-brand d-none d-md-flex align-items-center m-0 mr- 4 p-0 aabtn">
-                <img src="/images/LogoATE2.png" alt="Logo de la empresa"  class="Logo mr-1" id="LogoATE" width="250px">
-            </a>
-            <ul class="navbar-nav mr-auto align-rigth">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #aaeabf;">
+    <div class="container-fluid">
+        <a href="https://www.tmcorporacion.cl/" class="navbar-brand d-none d-md-flex align-items-center m-0 mr- 4 p-0 aabtn">
+            <img src="/images/LogoATE2.png" alt="Logo de la empresa"  class="Logo mr-1" id="LogoATE" width="250px">
+        </a>
+        <div class="collapse navbar-collapse Aling-content-rigth" id="navbarColor03">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/"><H6>INICIO</H6></a>
+                    <a class="nav-link" href="/">INICIO</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/nosotros"><H6>NOSOTROS</H6></a>
+                    <a class="nav-link" href="/nosotros">NOSOTROS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/contact"><H6>CONTACTANOS</H6></a>
+                    <a class="nav-link" href="/contact">CONTACTANOS</a>
                 </li>
             </ul>
         </div>
-    </nav>
+    </div>
+</nav>
